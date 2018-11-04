98cffcbba67cc5b5dd525d86a717848933300964
HP <HP@R1o-mu5>
2018-11-04 13:43:50 +0700

    menambahkan contructor, setter dan getter

--- src/mus/Remidial/Karyawan.java
+++ src/mus/Remidial/Karyawan.java
@@ -12,4 +12,27 @@ package mus.Remidial;
 public class Karyawan {
     private String idkaryawan;
     private String namakaryawan;
+
+    public Karyawan(String idkaryawan, String namakaryawan) {
+        this.idkaryawan = idkaryawan;
+        this.namakaryawan = namakaryawan;
+    }
+
+    public String getIdkaryawan() {
+        return idkaryawan;
+    }
+
+    public void setIdkaryawan(String idkaryawan) {
+        this.idkaryawan = idkaryawan;
+    }
+
+    public String getNamakaryawan() {
+        return namakaryawan;
+    }
+
+    public void setNamakaryawan(String namakaryawan) {
+        this.namakaryawan = namakaryawan;
+    }
+    
+    
 }
