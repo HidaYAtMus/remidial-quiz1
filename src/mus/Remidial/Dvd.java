44ca36b7e99f818a8e8c33de295b40ddb853f797
HP <HP@R1o-mu5>
2018-11-04 13:51:16 +0700

    Menambahkan atribut, contructor, setter, getter, dan method

--- src/mus/Remidial/Dvd.java
+++ src/mus/Remidial/Dvd.java
@@ -10,5 +10,69 @@ package mus.Remidial;
  * @author HP
  */
 public class Dvd {
+    private String kode;
+    private String namaDvd;
+    private Penyewa sewa;
+    private LamaSewa waktu;
+    private Karyawan karyawan;
+
+    public Dvd(String kode, String namaDvd, Penyewa sewa, LamaSewa waktu, Karyawan karyawan) {
+        this.kode = kode;
+        this.namaDvd = namaDvd;
+        this.sewa = sewa;
+        this.waktu = waktu;
+        this.karyawan = karyawan;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getNamaDvd() {
+        return namaDvd;
+    }
+
+    public void setNamaDvd(String namaDvd) {
+        this.namaDvd = namaDvd;
+    }
+
+    public Penyewa getSewa() {
+        return sewa;
+    }
+
+    public void setSewa(Penyewa sewa) {
+        this.sewa = sewa;
+    }
+
+    public LamaSewa getWaktu() {
+        return waktu;
+    }
+
+    public void setWaktu(LamaSewa waktu) {
+        this.waktu = waktu;
+    }
+
+    public Karyawan getKaryawan() {
+        return karyawan;
+    }
+
+    public void setKaryawan(Karyawan karyawan) {
+        this.karyawan = karyawan;
+    }
     
+    public String info(){
+        String info ="";
+        info += "Kode DVD : " + this.kode + "\n";
+        info += "Nama DVD : " + this.namaDvd + "\n"+"\n";
+        info += "Penyewa : " + "\n";
+        info += this.sewa.info();
+        info += this.waktu.info() + "\n" + "\n";
+        info += "Karyawan yang melayani : " + "\n";
+        info += this.karyawan.info() + "\n";
+        return info;
+    }
 }
