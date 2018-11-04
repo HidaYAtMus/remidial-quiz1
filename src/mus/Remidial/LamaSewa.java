ccd12f5f5b78bb1553173c21ce93e47bae7efdff
HP <HP@R1o-mu5>
2018-11-04 13:49:42 +0700

    Menambahkan atribut, contructor, stter,getter, dan ,method info

--- src/mus/Remidial/LamaSewa.java
+++ src/mus/Remidial/LamaSewa.java
@@ -10,5 +10,23 @@ package mus.Remidial;
  * @author HP
  */
 public class LamaSewa {
+    private int lamasewa;
+
+    public LamaSewa(int lamasewa) {
+        this.lamasewa = lamasewa;
+    }
+
+    public int getLamasewa() {
+        return lamasewa;
+    }
+
+    public void setLamasewa(int lamasewa) {
+        this.lamasewa = lamasewa;
+    }
     
+    public String info(){
+        String info ="";
+        info += "Lama sewa : " + this.lamasewa + "hari";
+        return info;
+    }
 }
